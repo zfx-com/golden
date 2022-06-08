e1290f9738e109854e37640c39464b02f5cb3c7e
cyrax <bskaxe@gmail.com>
2022-06-08 11:37:29 +0530

    add BrightnessContrast

--- lib/golden.dart
+++ lib/golden.dart
@@ -1,4 +1,6 @@
 library golden;
 
-export 'src/test_device_goldens.dart';
+export 'src/brightness_contrast.dart';
+export 'src/device.dart';
 export 'src/golden_tester.dart';
+export 'src/test_device_goldens.dart';
---
+++ lib/src/brightness_contrast.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BrightnessContrast {
+  BrightnessContrast({
+    required this.brightness,
+    required this.hightContrast,
+  });
+  final Brightness brightness;
+  final bool hightContrast;
+}
